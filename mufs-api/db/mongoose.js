1f160421dc8bdc8bb3c9253392c69c6a87dc7ebe
Mader13 <59108331+Mader13@users.noreply.github.com>
2022-10-09 21:09:59 +0600

    track package.jsom

--- mufs-api/db/mongoose.js
+++ mufs-api/db/mongoose.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://admin:admin@cluster0.oldqjdk.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 mongoose
   .connect("mongodb://localhost:27017/MUFS-db", {
     useNewUrlParser: true,
